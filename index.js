040dbcee7e4a1bc58e7cab5e56004c05b320a25c
jackrobertscott <mast.j.scott@gmail.com>
2015-10-11 22:10:03 +0800

    replace orchestrator methods with own code
    replaced methods to allow for passing options to tasks which could not
    be done before with orchestrator.

--- index.js
+++ index.js
@@ -4,42 +4,15 @@
 var fs = require('fs');
 var path = require('path');
 var chalk = require('chalk');
-var tasks = require('./lib/tasks');
-var tdir = path.join(__dirname, 'lib/tasks');
+var sip = require('./lib/sip');
 
+/**
+ * Load the task files.
+ */
+var tdir = path.join(__dirname, 'lib/tasks');
 fs.readdirSync(tdir)
   .forEach(function(file) {
     require(path.join(tdir, file));
   });
 
-module.exports = {
-  log: log,
-  has: has,
-  run: run,
-};
-
-/**
- * Log task information as they run.
- */
-function log() {
-  tasks.on('task_start', function(e) {
-    console.log(chalk.cyan('started:'), e.task);
-  });
-  tasks.on('task_start', function(e) {
-    console.log(chalk.cyan('finised:'), e.task);
-  });
-}
-
-/**
- * Check if a task exists.
- */
-function has(name) {
-  return tasks.hasTask(name);
-}
-
-/**
- * Run a task.
- */
-function run(name) {
-  tasks.start(name);
-}
+module.exports = sip;
