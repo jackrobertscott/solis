09aceec50684a7e7e3f6f7aa0950f6b91b465b2b
jackrobertscott <mast.j.scott@gmail.com>
2015-10-20 20:38:21 +0800

    add serve tasks (jade, less, sass/scss) #6

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 /* jshint node:true */
 'use strict';
 
-var fs = require('fs');
+var fs = require('fs-extra');
 var path = require('path');
 var chalk = require('chalk');
+var _ = require('lodash');
 var sip = require('./lib/sip');
+var solis = module.exports = {};
 
 /**
  * Load the task files.
@@ -16,20 +18,22 @@ fs.readdirSync(tdir)
   });
 
 /**
- * Initialise solis object
- */
-var solis = module.exports = {};
-
-/**
  * Serve files and reload on changes
  */
 solis.serve = function serve(opts) {
-  sip.run('serve', opts);
+  sip.run('serve', _.defaults(opts, {
+    src: path.join(process.cwd(), 'src'),
+    tmp: path.join(process.cwd(), '.tmp'),
+    quiet: false,
+    sourcemaps: false,
+  }));
 };
 
 /**
  * Generate files from templates
  */
 solis.generate = function generate(opts) {
-  sip.run('generate', opts);
+  sip.run('generate', _.defaults(opts, {
+    quiet: false,
+  }));
 };
